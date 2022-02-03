fdb445e34c6f226c723f29205b26fe8ca51c3ff9
udemy prueba <udemyprueba@domain.com>
2022-02-03 13:37:40 +0100

    add integrating test using mockito, junit spring framework test

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-26T09:09:50+0100",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_212 (Oracle Corporation)"
+    date = "2022-02-03T12:30:58+0100",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.13 (Ubuntu)"
 )
 @Component
 public class InvoiceResposeMapperImpl implements InvoiceResposeMapper {
